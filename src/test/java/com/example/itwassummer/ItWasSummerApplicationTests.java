705ce35d453b26833564e9684dba4b7381bf0b08
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-04 19:30:51 +0900

    [init] 최초 환경 설정 (+PR 템플릿)

---
+++ src/test/java/com/example/itwassummer/ItWasSummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itwassummer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItWasSummerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
