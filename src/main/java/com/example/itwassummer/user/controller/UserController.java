786e46d7f36ceefa83b5477acd8cf68b27221e2e
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-06 20:12:39 +0900

    [complete] Swagger 적용하기

--- src/main/java/com/example/itwassummer/user/controller/UserController.java
+++ src/main/java/com/example/itwassummer/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.itwassummer.user.controller;
 import com.example.itwassummer.common.dto.ApiResponseDto;
 import com.example.itwassummer.user.dto.SignupRequestDto;
 import com.example.itwassummer.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "사용자 API", description = "사용자의 회원 가입 기능과 관련된 API 정보를 담고 있습니다.")
 public class UserController {
     private final UserService userService;
 
@@ -20,6 +23,8 @@ public class UserController {
         this.userService = userService;
     }
 
+
+    @Operation(summary = "회원 가입", description = "SignupRequesetDto를 통해 회원이 제출한 정보의 유효성 검사 후 통과 시 DB에 저장하고 성공 메시지를 반환합니다.")
     @PostMapping("/signup")
     public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) throws IllegalAccessException {
         userService.signup(requestDto);
