8276621b6bed5bc1372e1fc1bbd10bb9168a50e4
Hyeon woo Kim <wooin.dev@gmail.com>
2023-08-07 17:07:51 +0900

    [init] Comment 필요 파일 생성
    Card 도메인 머지 후 추가 작업 예정

---
+++ src/main/java/com/example/itwassummer/comment/dto/CommentResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.itwassummer.comment.dto;
+
+import com.example.itwassummer.comment.entity.Comment;
+
+import java.time.LocalDateTime;
+
+public class CommentResponseDto {
+    private Long id;
+    private String nickname;
+    private String content;
+
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.nickname = comment.getUser().getnickname();
+        this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+    }
+}
