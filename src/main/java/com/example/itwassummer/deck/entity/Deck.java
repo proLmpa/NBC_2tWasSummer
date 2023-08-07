4ac9cab5b970273a7bc22c4673b02756479014a8
JisooPyo <audwl326@gmail.com>
2023-08-07 23:37:07 +0900

    [init] DeckEntity 구현

--- src/main/java/com/example/itwassummer/deck/entity/Deck.java
+++ src/main/java/com/example/itwassummer/deck/entity/Deck.java
@@ -1,5 +1,6 @@
 package com.example.itwassummer.deck.entity;
 
+import com.example.itwassummer.board.entity.Board;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,19 @@ import lombok.NoArgsConstructor;
 public class Deck {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "deck_id")
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER) // 덱은 항상 보드와 같이 조회되므로
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @OneToOne(fetch = FetchType.EAGER) // 덱들은 항상 같이 조회되므로
+    @JoinColumn(name="parent_id")
+    private Deck deck;
+
+    private Boolean is_deleted;
+
 }
