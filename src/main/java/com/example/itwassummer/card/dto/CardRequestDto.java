d3da9042eade8ef8403d11c1447a712c13f70002
aiva <42510512+sxi8022@users.noreply.github.com>
2023-08-07 17:32:46 +0900

    [init] 카드 엔티티 구현 (다른 테이블과 관계는 추후에 구현) (#21)
    - 컨트롤러 테스트 코드를 통해 카드 데이터 등록 가능
    - 수정 삭제 메소드 작업 중

---
+++ src/main/java/com/example/itwassummer/card/dto/CardRequestDto.java
@@ -0,0 +1,36 @@
+package com.example.itwassummer.card.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardRequestDto {
+
+
+  // 카드 이름
+  @NotBlank
+  private String name;
+
+  // 만기일
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime dueDate;
+
+  // 설명
+  private String description;
+
+  // 정렬순서
+  private Long parentId;
+
+
+
+
+}
