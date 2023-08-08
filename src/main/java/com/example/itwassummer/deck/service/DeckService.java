569f135c189669b8466aff5e9afabab0e90ec903
JisooPyo <audwl326@gmail.com>
2023-08-09 00:36:03 +0900

    [update] DeckService - createDeck() 주석 추가

--- src/main/java/com/example/itwassummer/deck/service/DeckService.java
+++ src/main/java/com/example/itwassummer/deck/service/DeckService.java
@@ -4,5 +4,9 @@ import com.example.itwassummer.deck.dto.DeckRequestDto;
 import com.example.itwassummer.deck.dto.DeckResponseDto;
 
 public interface DeckService {
+	/*
+		@param boardId, requestDto(덱 등록 요청 정보)
+		@return DeckResponseDto(덱 정보)
+	 */
 	DeckResponseDto createDeck(Long boardId, DeckRequestDto requestDto);
 }
