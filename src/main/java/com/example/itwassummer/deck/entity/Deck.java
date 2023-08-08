d48c5075555a67d8c76bd2e3c1da6c208f00270e
JisooPyo <audwl326@gmail.com>
2023-08-08 15:22:35 +0900

    [update] Deck 타임스탬프 상속, 생성자, updateParent() 메서드 추가

--- src/main/java/com/example/itwassummer/deck/entity/Deck.java
+++ src/main/java/com/example/itwassummer/deck/entity/Deck.java
@@ -1,6 +1,7 @@
 package com.example.itwassummer.deck.entity;
 
 import com.example.itwassummer.board.entity.Board;
+import com.example.itwassummer.common.entity.Timestamped;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,22 +10,30 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Table(name = "deck")
-public class Deck {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Deck extends Timestamped {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String name;
+	@Column(nullable = false)
+	private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER) // 덱은 항상 보드와 같이 조회되므로
-    @JoinColumn(name = "board_id")
-    private Board board;
+	@ManyToOne(fetch = FetchType.EAGER) // 덱은 항상 보드와 같이 조회되므로
+	@JoinColumn(name = "board_id")
+	private Board board;
 
-    @OneToOne(fetch = FetchType.EAGER) // 덱들은 항상 같이 조회되므로
-    @JoinColumn(name="parent_id")
-    private Deck parent;
+	@OneToOne(fetch = FetchType.EAGER) // 덱들은 항상 같이 조회되므로
+	@JoinColumn(name = "parent_id")
+	private Deck parent;
 
-    private Boolean is_deleted;
+	private Boolean is_deleted;
 
+	public Deck(String name, Board board) {
+		this.name = name;
+		this.board = board;
+	}
+
+	public void updateParent(Deck parent) {
+		this.parent = parent;
+	}
 }
