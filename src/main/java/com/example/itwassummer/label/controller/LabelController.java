10fd628ee3ef028624e1372b9ca5405d558c59e6
Hyeon woo Kim <133616377+Wooin-dev@users.noreply.github.com>
2023-08-08 21:17:07 +0900

    [init] Label 도메인 기본 파일 생성 ( (#25)
    * [init] Lable 도메인 기본 파일 생성 (코드없음)
    
    * [init] Label Entity 클래스 생성
    
    * [init] Label 도메인 기본 기능 구현
    
    **todo**
    Card 객체통해서 테스트
    Board 기능 머지 후 테스트
    
    * [init] Label 도메인 기본 기능 구현
    
    **todo**
    Card 객체통해서 테스트
    Board 기능 머지 후 테스트
    
    * [refactor] service 주석추가
    
    * [refactor] controller uri 수정
    
    * [refactor] Transactional 수정
    
    조회에는 readOnly=true,
    생성, 수정, 삭제에도 @Transactional 추가

---
+++ src/main/java/com/example/itwassummer/label/controller/LabelController.java
@@ -0,0 +1,63 @@
+package com.example.itwassummer.label.controller;
+
+import com.example.itwassummer.common.dto.ApiResponseDto;
+import com.example.itwassummer.label.dto.LabelCreateRequestDto;
+import com.example.itwassummer.label.dto.LabelEditRequestDto;
+import com.example.itwassummer.label.dto.LabelResponseDto;
+import com.example.itwassummer.label.service.LabelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Tag(name = "라벨 API", description = "카드에 태그형태로 붙는 라벨 API 정보들을 담고 있습니다.")
+public class LabelController {
+
+    private final LabelService labelService;
+
+
+    @PostMapping("/labels")
+    @Operation(summary = "새로운 라벨 생성", description = "라벨의 이름과 색상 정보를 받아 새로운 라벨을 생성합니다.")
+    public ResponseEntity<ApiResponseDto> createLabel(LabelCreateRequestDto requestDto, @RequestParam("boardId") Long boardId) {
+
+        String result = labelService.createLabel(requestDto, boardId);
+
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), result));
+    }
+
+    @GetMapping("/labels")
+    @Operation(summary = "라벨 리스트 조회", description = "라벨의 이름과 색상 정보를 받아 새로운 라벨을 생성합니다.")
+    public ResponseEntity<List<LabelResponseDto>> getLabels(@RequestParam Long boardId) {
+
+        List<LabelResponseDto> labelResponseDtos = labelService.getLabels(boardId);
+
+        return ResponseEntity.ok().body(labelResponseDtos);
+    }
+
+    @PutMapping("/labels/{labelId}")
+    @Operation(summary = "라벨 정보 수정", description = "보드 내에 생성되어 있는 라벨의 정보를 변경합니다.")
+    public ResponseEntity<ApiResponseDto> editLabel(@PathVariable Long labelId, LabelEditRequestDto requestDto) {
+
+        String result = labelService.editLabel(labelId, requestDto);
+
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), result));
+    }
+
+    @DeleteMapping("/labels/{labelId}")
+    @Operation(summary = "라벨 삭제", description = "보드 내에 생성되어 있는 라벨을 삭제합니다.")
+    public ResponseEntity<ApiResponseDto> deleteLabel(@PathVariable Long labelId) {
+
+        String result = labelService.deleteLabel(labelId);
+
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), result));
+    }
+
+}
