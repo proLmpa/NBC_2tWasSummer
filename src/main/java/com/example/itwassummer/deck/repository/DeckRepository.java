9a845201f710022d8da816304bb59506c7530ed1
JisooPyo <audwl326@gmail.com>
2023-08-08 18:35:55 +0900

    [update] DeckRepository에 DeckService에 필요한 메서드 구현

--- src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
+++ src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
@@ -4,6 +4,12 @@ import com.example.itwassummer.deck.entity.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface DeckRepository extends JpaRepository<Deck, Long> {
+	Deck findByParentNull();
+
+	Deck findByParent(Deck parent);
 }
