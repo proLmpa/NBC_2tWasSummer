0b136cd482d3a6085c2b8e7544f39d50e208828d
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-10 00:59:54 +0900

    [complete] 보드에 사용자 초대/제거 기능 구현

--- src/main/java/com/example/itwassummer/boardmember/service/BoardMemberServiceImpl.java
+++ src/main/java/com/example/itwassummer/boardmember/service/BoardMemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.itwassummer.boardmember.service;
 
 import com.example.itwassummer.board.entity.Board;
+import com.example.itwassummer.board.repository.BoardRepository;
 import com.example.itwassummer.boardmember.entity.BoardMember;
 import com.example.itwassummer.boardmember.repository.BoardMemberRepository;
 import com.example.itwassummer.common.error.CustomErrorCode;
@@ -16,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BoardMemberServiceImpl implements BoardMemberService {
     private final UserRepository userRepository;
-    //    private final BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
     private final BoardMemberRepository boardMemberRepository;
 
     @Override
@@ -24,20 +25,16 @@ public class BoardMemberServiceImpl implements BoardMemberService {
     public void inviteBoardMember(Long boardId, Long userId, User user) {
         Board board = findBoard(boardId);
         User invitee = findUser(userId);
+        confirmUser(board, user);
 
-        if (matchOwner(board, user) || isAdmin(user)) {
-            BoardMember boardMember = boardMemberRepository.findByBoardAndUser(board, invitee).orElse(null);
+        BoardMember boardMember = boardMemberRepository.findByBoardAndUser(board, invitee).orElse(null);
 
-            if (boardMember == null) {
-                boardMember = new BoardMember(invitee, board);
-                boardMemberRepository.save(boardMember);
-            } else {
-                throw new CustomException(CustomErrorCode.BOARD_MEMBER_ALREADY_EXISTS, null);
-            }
+        if (boardMember == null) {
+            boardMember = new BoardMember(invitee, board);
+            boardMemberRepository.save(boardMember);
         } else {
-            throw new CustomException(CustomErrorCode.UNAUTHORIZED_REQUEST, null);
+            throw new CustomException(CustomErrorCode.BOARD_MEMBER_ALREADY_EXISTS, null);
         }
-
     }
 
     @Override
@@ -46,23 +43,19 @@ public class BoardMemberServiceImpl implements BoardMemberService {
         Board board = findBoard(boardId);
         User invitee = findUser(userId);
 
-        if (matchOwner(board, user) || isAdmin(user)) {
-            BoardMember boardMember = boardMemberRepository.findByBoardAndUser(board, invitee).orElse(null);
-            if (boardMember != null) {
-                boardMember.deleteBoardMember(invitee, board);
-                boardMemberRepository.deleteById(boardMember.getId());
-            } else {
-                throw new CustomException(CustomErrorCode.BOARD_MEMBER_NOT_EXISTS, null);
-            }
+        confirmUser(board, user);
+
+        BoardMember boardMember = boardMemberRepository.findByBoardAndUser(board, invitee).orElse(null);
+        if (boardMember != null) {
+            boardMemberRepository.deleteById(boardMember.getId());
         } else {
-            throw new CustomException(CustomErrorCode.UNAUTHORIZED_REQUEST, null);
+            throw new CustomException(CustomErrorCode.BOARD_MEMBER_NOT_EXISTS, null);
         }
     }
 
     private Board findBoard(Long boardId) {
-        return null;
-//        return boardRepository.findById(boardId).orElseThrow(()
-//                -> new CustomException(CustomErrorCode.BOARD_NOT_FOUND, null));
+        return boardRepository.findById(boardId).orElseThrow(()
+                -> new CustomException(CustomErrorCode.BOARD_NOT_FOUND, null));
     } // 해당 ErrorCode가 존재하지 않으면 CustomErrorCode에 추가하셔야 합니다.
 
     private User findUser(Long userId) {
@@ -70,13 +63,9 @@ public class BoardMemberServiceImpl implements BoardMemberService {
                 -> new CustomException(CustomErrorCode.USER_NOT_FOUND, null));
     }
 
-    private boolean matchOwner(Board board, User user) {
-        return false;
-//        return board.getUser().getId() == user.getId();
-        // board에 user 연관관계를 매핑한 후에 getUser()를 사용 가능합니다.
-    }
-
-    private boolean isAdmin(User user) {
-        return user.getRole() == UserRoleEnum.ADMIN;
+    private void confirmUser(Board board, User user) {
+        if(!board.getUser().getId().equals(user.getId())
+            && user.getRole().equals(UserRoleEnum.ADMIN))
+            throw new CustomException(CustomErrorCode.UNAUTHORIZED_REQUEST, null);
     }
 }
