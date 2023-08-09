1834493e63b9a307d630f9a8114499f1775a8a11
JisooPyo <audwl326@gmail.com>
2023-08-10 01:40:50 +0900

    [update] createDeck() 메서드 변경에 따른 DeckRepository update

--- src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
+++ src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
@@ -4,12 +4,7 @@ import com.example.itwassummer.deck.entity.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
-
 @Repository
-public interface DeckRepository extends JpaRepository<Deck, Long> {
-	Deck findByParentNull();
+public interface DeckRepository extends JpaRepository<Deck, Long>, DeckCustomRepository{
 
-	Deck findByParent(Deck parent);
 }
