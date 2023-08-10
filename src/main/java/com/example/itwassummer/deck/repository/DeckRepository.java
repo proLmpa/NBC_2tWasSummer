7c4c40335c2b81ad8b28d915462e79defda45e8a
JisooPyo <audwl326@gmail.com>
2023-08-11 02:27:00 +0900

    [refactor] 안 쓰는 메서드 삭제

--- src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
+++ src/main/java/com/example/itwassummer/deck/repository/DeckRepository.java
@@ -1,13 +1,10 @@
 package com.example.itwassummer.deck.repository;
 
-import com.example.itwassummer.board.entity.Board;
 import com.example.itwassummer.deck.entity.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeckRepository extends JpaRepository<Deck, Long>, DeckCustomRepository{
-	Deck findByParentNullAndBoardAndIsDeletedFalse(Board board);
-
 	Deck findByParentAndIsDeletedFalse(Deck deck);
 }
