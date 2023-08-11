11b4bacb35b8291fd74cdc83e0f87b03defd3a5c
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-11 09:59:46 +0900

    [refactor] Label Service 인터페이스 주석 개선

--- src/main/java/com/example/itwassummer/label/service/LabelService.java
+++ src/main/java/com/example/itwassummer/label/service/LabelService.java
@@ -8,7 +8,6 @@ import java.util.List;
 public interface LabelService {
     /**
      * 해당 보드에 생성되어 있는 모든 라벨 조회.
-     *
      * @param boardId 보드 Id
      * @return 라벨의 정보들을 담은 ResponseDto들의 List
      */
@@ -16,23 +15,22 @@ public interface LabelService {
 
     /**
      * 해당 보드 안에서 사용될 라벨을 생성. (카드에 라벨 등록 아님)
-     *
      * @param requestDto 라벨 생성에 필요한 정보. title, color코드
      * @param boardId    라벨이 생성될 보드의 Id
+     * @return responseDto 생성된 라벨 정보 반환
      */
     LabelResponseDto createLabel(LabelRequestDto requestDto, Long boardId);
 
     /**
      * 해당 라벨의 정보를 수정.
-     *
      * @param labelId    라벨의 Id값
      * @param requestDto 라벨의 수정하고자 하는 정보.
+     * @return responseDto 수정된 라벨 정보 반환
      */
     LabelResponseDto editLabel(Long labelId, LabelRequestDto requestDto);
 
     /**
      * 해당 라벨을 삭제.
-     *
      * @param labelId 라벨의 Id값
      */
     void deleteLabel(Long labelId);
