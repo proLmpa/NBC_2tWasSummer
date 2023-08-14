b768ebc8224a17fa1cb91771b76133d2b1262964
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-14 12:44:47 +0900

    [refactor] Card API 리팩토링

--- src/main/java/com/example/itwassummer/card/controller/CardController.java
+++ src/main/java/com/example/itwassummer/card/controller/CardController.java
@@ -1,163 +1,145 @@
 package com.example.itwassummer.card.controller;
 
-
-import com.example.itwassummer.card.dto.CardListResponseDto;
-import com.example.itwassummer.card.dto.CardRequestDto;
-import com.example.itwassummer.card.dto.CardResponseDto;
-import com.example.itwassummer.card.dto.CardSearchResponseDto;
-import com.example.itwassummer.card.dto.CardViewResponseDto;
+import com.example.itwassummer.card.dto.*;
 import com.example.itwassummer.card.service.CardService;
 import com.example.itwassummer.cardmember.dto.CardMemberResponseDto;
-import com.example.itwassummer.comment.dto.CommentResponseDto;
 import com.example.itwassummer.common.dto.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
 @Tag(name = "카드 API", description = "카드 기능과 관련된 API 정보를 담고 있습니다.")
 public class CardController {
 
-  public CardController(CardService cardService) {
-    this.cardService = cardService;
-  }
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
 
-  private final CardService cardService;
+    private final CardService cardService;
 
-  @Operation(summary = "카드 전체조회", description = "카드 id를 넘겨 받아 카드의 상세 정보를 표시")
-  @GetMapping(value = "/cardLists")
-  @ResponseBody
-  public ResponseEntity list(@RequestParam Long boardId,
-      @RequestParam("page") int page,
-      @RequestParam("size") int size,
-      @RequestParam("sortBy") String sortBy,
-      @RequestParam("isAsc") boolean isAsc) {
-    List<CardListResponseDto> responseDto = cardService.getCardList(boardId,
-        page - 1, size, sortBy, isAsc);
-    return new ResponseEntity<>(responseDto, HttpStatus.OK);
-  }
-
-
-  @Operation(summary = "카드 상세조회", description = "카드 id를 넘겨 받아 카드의 상세 정보를 표시")
-  @GetMapping(value = "/cards/{cardId}")
-  @ResponseBody
-  public ResponseEntity view(@PathVariable("cardId") Long cardId) {
-    CardViewResponseDto responseDto = cardService.getCard(cardId);
-    return new ResponseEntity<>(responseDto, HttpStatus.OK);
-  }
-
-  @Operation(summary = "카드 등록", description = "CardRequestDto를 통해 카드정보를 전달 받은 후 DB에 저장하고 성공 메시지를 반환합니다.")
-  @PostMapping(value = "/cards", consumes = {MediaType.APPLICATION_JSON_VALUE,
-      MediaType.MULTIPART_FORM_DATA_VALUE})
-  public ResponseEntity addCard(@Valid @RequestPart CardRequestDto requestDto,
-      @RequestPart(required = false) List<MultipartFile> files
-  ) throws IOException {
-    CardResponseDto returnDto = cardService.save(requestDto, files);
-    return new ResponseEntity<>(returnDto, HttpStatus.OK);
-  }
-
-  @Operation(summary = "카드 수정", description = "CardRequestDto를 통해 카드정보를 전달 받은 후 DB에 저장하고 성공 메시지를 반환합니다.")
-  @PutMapping(value = "/cards/{cardId}", consumes = {MediaType.APPLICATION_JSON_VALUE,
-      MediaType.MULTIPART_FORM_DATA_VALUE})
-  public ResponseEntity updateCard(@PathVariable("cardId") Long cardId,
-      @Valid @RequestPart CardRequestDto requestDto,
-      @RequestPart(required = false) List<MultipartFile> files
-  ) throws IOException {
-    CardResponseDto returnDto = cardService.update(cardId, requestDto, files);
-    return new ResponseEntity<>(returnDto, HttpStatus.OK);
-  }
-
-  @Operation(summary = "카드 삭제", description = "id 값을 통해 삭제")
-  @DeleteMapping("/cards/{cardId}")
-  public ResponseEntity<ApiResponseDto> deleteCard(@PathVariable("cardId") Long cardId) {
-    cardService.delete(cardId);
-    return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "삭제 성공"));
-  }
+    @Operation(summary = "카드 전체조회", description = "카드 id를 넘겨 받아 카드의 상세 정보를 표시")
+    @GetMapping(value = "/cards")
+    @ResponseBody
+    public ResponseEntity list(@RequestParam Long boardId,
+                               @RequestParam("page") int page,
+                               @RequestParam("size") int size,
+                               @RequestParam("sortBy") String sortBy,
+                               @RequestParam("isAsc") boolean isAsc) {
+        List<CardListResponseDto> responseDto = cardService.getCardList(boardId,
+                page - 1, size, sortBy, isAsc);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
 
 
-  @Operation(summary = "카드 마감일수정", description = "마감일 수정")
-  @PatchMapping("/cards/{cardId}/date")
-  public ResponseEntity changeCardDueDate(
-      @PathVariable("cardId") Long cardId,
-      @RequestParam String dueDate
-  )
-      throws ParseException {
+    @Operation(summary = "카드 상세조회", description = "카드 id를 넘겨 받아 카드의 상세 정보를 표시")
+    @GetMapping(value = "/cards/{cardId}")
+    @ResponseBody
+    public ResponseEntity view(@PathVariable("cardId") Long cardId) {
+        CardViewResponseDto responseDto = cardService.getCard(cardId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
 
-    SimpleDateFormat dateFormatParser = new SimpleDateFormat(
-        "yyyy-MM-dd'T'HH:mm:ss"); //검증할 날짜 포맷 설정
-    dateFormatParser.setLenient(false); //false일경우 처리시 입력한 값이 잘못된 형식일 시 오류가 발생
-    dateFormatParser.parse(dueDate); //대상 값 포맷에 적용되는지 확인
+    @Operation(summary = "카드 등록", description = "CardRequestDto를 통해 카드정보를 전달 받은 후 DB에 저장하고 성공 메시지를 반환합니다.")
+    @PostMapping(value = "/cards", consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity addCard(@Valid @RequestPart CardRequestDto requestDto,
+                                  @RequestPart(required = false) List<MultipartFile> files
+    ) throws IOException {
+        CardResponseDto returnDto = cardService.save(requestDto, files);
+        return new ResponseEntity<>(returnDto, HttpStatus.OK);
+    }
 
-    CardResponseDto responseDto = cardService.changeDueDate(cardId, dueDate);
-    return new ResponseEntity<>(responseDto, HttpStatus.OK);
-  }
+    @Operation(summary = "카드 수정", description = "CardRequestDto를 통해 카드정보를 전달 받은 후 DB에 저장하고 성공 메시지를 반환합니다.")
+    @PutMapping(value = "/cards/{cardId}", consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity updateCard(@PathVariable("cardId") Long cardId,
+                                     @Valid @RequestPart CardRequestDto requestDto,
+                                     @RequestPart(required = false) List<MultipartFile> files
+    ) throws IOException {
+        CardResponseDto returnDto = cardService.update(cardId, requestDto, files);
+        return new ResponseEntity<>(returnDto, HttpStatus.OK);
+    }
 
-  @Operation(summary = "카드별 사용자 변경", description = "카드별 사용자 일괄 삭제 후 재등록")
-  @PatchMapping("/cards/{cardId}/members")
-  public ResponseEntity changeCardMember(
-      @PathVariable("cardId") Long cardId,
-      @RequestParam String emailList
-  )
-      throws IllegalAccessException {
-    List<CardMemberResponseDto> result = cardService.changeCardMembers(cardId, emailList);
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
+    @Operation(summary = "카드 삭제", description = "id 값을 통해 삭제")
+    @DeleteMapping("/cards/{cardId}")
+    public ResponseEntity<ApiResponseDto> deleteCard(@PathVariable("cardId") Long cardId) {
+        cardService.delete(cardId);
+        return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "삭제 성공"));
+    }
 
-  @Operation(summary = "카드 정렬 순서 바꾸기", description = "카드 정렬 순서 바꾸기")
-  @PatchMapping("/cards/{cardId}/move")
-  public ResponseEntity moveCard(
-      @PathVariable("cardId") Long cardId,
-      @RequestParam Long order
-  )
-      throws IllegalAccessException {
-    CardResponseDto result = cardService.moveCard(cardId, order);
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
 
-  @Operation(summary = "라벨 필터검색", description = "id 값을 통해 조회")
-  @GetMapping("/cards/labels/{labelId}")
-  @ResponseBody
-  public ResponseEntity searchLabelList(
-      @PathVariable("labelId") Long labelId,
-      @RequestParam("page") int page,
-      @RequestParam("size") int size,
-      @RequestParam("sortBy") String sortBy,
-      @RequestParam("isAsc") boolean isAsc
-  ) {
-    List<CardSearchResponseDto> cardList = cardService.searchLabelList(labelId,
-        page - 1, size, sortBy, isAsc);
+    @Operation(summary = "카드 마감일수정", description = "마감일 수정")
+    @PatchMapping("/cards/{cardId}/date")
+    public ResponseEntity changeCardDueDate(
+            @PathVariable("cardId") Long cardId,
+            @RequestParam String dueDate
+    )
+            throws ParseException {
 
-    return new ResponseEntity<>(cardList, HttpStatus.OK);
-  }
+        SimpleDateFormat dateFormatParser = new SimpleDateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss"); //검증할 날짜 포맷 설정
+        dateFormatParser.setLenient(false); //false일경우 처리시 입력한 값이 잘못된 형식일 시 오류가 발생
+        dateFormatParser.parse(dueDate); //대상 값 포맷에 적용되는지 확인
 
-  @Operation(summary = "카드 위치 수정", description = "덱 id와 카드 id, 정렬순서를 읽어 다른 덱으로 이동")
-  @PutMapping(value = "/decks/{deckId}/cards/{cardId}")
-  public ResponseEntity moveCardToOtherDeck(@PathVariable("deckId") Long deckId,
-      @PathVariable("cardId") Long cardId,
-      @RequestParam("order") Long order
-  ) {
-    CardResponseDto returnDto = cardService.moveCardToOtherDeck(deckId, cardId, order);
-    return new ResponseEntity<>(returnDto, HttpStatus.OK);
-  }
+        CardResponseDto responseDto = cardService.changeDueDate(cardId, dueDate);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
 
+    @Operation(summary = "카드별 사용자 변경", description = "카드별 사용자 일괄 삭제 후 재등록")
+    @PatchMapping("/cards/{cardId}/members")
+    public ResponseEntity changeCardMember(
+            @PathVariable("cardId") Long cardId,
+            @RequestParam String emailList
+    ) {
+        List<CardMemberResponseDto> result = cardService.changeCardMembers(cardId, emailList);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @Operation(summary = "카드 정렬 순서 바꾸기", description = "카드 정렬 순서 바꾸기")
+    @PatchMapping("/cards/{cardId}/move")
+    public ResponseEntity moveCard(
+            @PathVariable("cardId") Long cardId,
+            @RequestParam Long order
+    ) {
+        CardResponseDto result = cardService.moveCard(cardId, order);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @Operation(summary = "카드 위치 수정", description = "덱 id와 카드 id, 정렬순서를 읽어 다른 덱으로 이동")
+    @PutMapping(value = "/decks/{deckId}/cards/{cardId}")
+    public ResponseEntity moveCardToOtherDeck(@PathVariable("deckId") Long deckId,
+                                              @PathVariable("cardId") Long cardId,
+                                              @RequestParam("order") Long order
+    ) {
+        CardResponseDto returnDto = cardService.moveCardToOtherDeck(deckId, cardId, order);
+        return new ResponseEntity<>(returnDto, HttpStatus.OK);
+    }
+
+    @Operation(summary = "라벨 필터검색", description = "id 값을 통해 조회")
+    @GetMapping("/cards/labels/{labelId}")
+    @ResponseBody
+    public ResponseEntity searchLabelList(
+            @PathVariable("labelId") Long labelId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        List<CardSearchResponseDto> cardList = cardService.searchLabelList(labelId,
+                page - 1, size, sortBy, isAsc);
+
+        return new ResponseEntity<>(cardList, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/itwassummer/card/service/CardServiceImpl.java
+++ src/main/java/com/example/itwassummer/card/service/CardServiceImpl.java
@@ -2,19 +2,13 @@ package com.example.itwassummer.card.service;
 
 import com.example.itwassummer.board.entity.Board;
 import com.example.itwassummer.board.repository.BoardRepository;
-import com.example.itwassummer.card.dto.CardListResponseDto;
-import com.example.itwassummer.card.dto.CardRequestDto;
-import com.example.itwassummer.card.dto.CardResponseDto;
-import com.example.itwassummer.card.dto.CardSearchResponseDto;
-import com.example.itwassummer.card.dto.CardViewResponseDto;
+import com.example.itwassummer.card.dto.*;
 import com.example.itwassummer.card.entity.Card;
 import com.example.itwassummer.card.repository.CardRepository;
 import com.example.itwassummer.cardmember.dto.CardMemberResponseDto;
 import com.example.itwassummer.cardmember.entity.CardMember;
 import com.example.itwassummer.cardmember.repository.CardMemberRepository;
 import com.example.itwassummer.checklist.service.CheckListService;
-import com.example.itwassummer.comment.dto.CommentResponseDto;
-import com.example.itwassummer.comment.repository.CommentRepository;
 import com.example.itwassummer.common.error.CustomErrorCode;
 import com.example.itwassummer.common.exception.CustomException;
 import com.example.itwassummer.common.file.FileUploader;
@@ -23,12 +17,6 @@ import com.example.itwassummer.deck.entity.Deck;
 import com.example.itwassummer.deck.repository.DeckRepository;
 import com.example.itwassummer.user.entity.User;
 import com.example.itwassummer.user.repository.UserRepository;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -38,207 +26,207 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CardServiceImpl implements CardService {
 
-  private final CardRepository cardRepository;
+    private final CardRepository cardRepository;
 
-  private final FileUploader fileUploader;
+    private final FileUploader fileUploader;
 
-  private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-  private final CardMemberRepository cardMemberRepository;
+    private final CardMemberRepository cardMemberRepository;
 
-  private final CheckListService checkListService;
+    private final CheckListService checkListService;
 
-  private final CommentRepository commentRepository;
+    private final DeckRepository deckRepository;
 
-  private final DeckRepository deckRepository;
+    private final BoardRepository boardRepository;
 
-  private final BoardRepository boardRepository;
+    @Override
+    public List<CardListResponseDto> getCardList(Long boardId, int page, int size, String sortBy, boolean isAsc) {
+        findBoard(boardId);
 
-  @Override
-  public List<CardListResponseDto> getCardList(Long boardId, int page, int size, String sortBy,
-      boolean isAsc) {
-    Board board = findBoard(boardId);
+        Direction direction = isAsc ? Direction.ASC : Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
 
-    Direction direction = isAsc ? Direction.ASC : Direction.DESC;
-    Sort sort = Sort.by(direction, sortBy);
-    Pageable pageable = PageRequest.of(page, size, sort);
-    List<CardListResponseDto> lists = cardRepository.findAllByBoardId(boardId, pageable);
-    return lists;
-  }
-
-  @Override
-  @Transactional(readOnly = true)
-  public CardViewResponseDto getCard(Long cardId) {
-    Card card = this.findCard(cardId);
-    CardViewResponseDto responseDto = new CardViewResponseDto(card);
-    return responseDto;
-  }
-
-  @Override
-  @Transactional
-  public CardResponseDto save(CardRequestDto requestDto, List<MultipartFile> files)
-      throws IOException {
-
-    Deck deck = findDeck(requestDto.getDeckId());
-
-    // 파일 등록
-    List<S3FileDto> fileDtoList = null;
-    if (!(files == null || (files.size() == 1 && files.get(0).isEmpty()))) {
-      fileDtoList = fileUploader.uploadFiles(files, "files");
-      requestDto.setAttachment(fileDtoList);
+        return cardRepository.findAllByBoardId(boardId, pageable);
     }
 
-    Card card = Card.builder()
-        .requestDto(requestDto)
-        .deck(deck)
-        .build();
-    Card returnCard = cardRepository.save(card);
-    CardResponseDto responseDto = new CardResponseDto(returnCard);
-    return responseDto;
-  }
+    @Override
+    @Transactional(readOnly = true)
+    public CardViewResponseDto getCard(Long cardId) {
+        Card card = this.findCard(cardId);
 
-  @Override
-  @Transactional
-  public CardResponseDto update(Long cardId, CardRequestDto requestDto, List<MultipartFile> files)
-      throws IOException {
-    // 파일첨부
-    List<S3FileDto> fileDtoList = null;
-    Card card = findCard(cardId);
-
-    // 파일정보 불러오기
-    List<S3FileDto> attachment = card.getAttachment();
-
-    // 기존 파일 삭제
-    if (attachment != null && attachment.size() > 0) {
-      for (int i = 0; i < attachment.size(); i++) {
-        fileUploader.deleteFile(attachment.get(i).getUploadFilePath(),
-            attachment.get(i).getUploadFileName());
-      }
+        return new CardViewResponseDto(card);
     }
 
-    // 파일 등록 
-    if (!(files == null || (files.size() == 1 && files.get(0).isEmpty()))) {
-      fileDtoList = fileUploader.uploadFiles(files, "files");
-      requestDto.setAttachment(fileDtoList);
-    }
+    @Override
+    @Transactional
+    public CardResponseDto save(CardRequestDto requestDto, List<MultipartFile> files)
+            throws IOException {
 
-    // card 내용 수정
-    card.update(requestDto);
-    CardResponseDto responseDto = new CardResponseDto(card);
+        Deck deck = findDeck(requestDto.getDeckId());
 
-    return responseDto;
-  }
-
-  @Override
-  @Transactional
-  public void delete(Long cardId) {
-    Card card = findCard(cardId);
-
-    // 파일정보 불러오기
-    List<S3FileDto> attachment = card.getAttachment();
-
-    // 기존 파일 삭제
-    if (attachment != null && attachment.size() > 0) {
-      for (int i = 0; i < attachment.size(); i++) {
-        fileUploader.deleteFile(attachment.get(i).getUploadFilePath(),
-            attachment.get(i).getUploadFileName());
-      }
-    }
-    //기존 체크리스트 삭제
-    for (int i = 0; i < card.getCheckLists().size(); i++) {
-      checkListService.delete(card.getCheckLists().get(i).getId());
-    }
-
-    cardRepository.deleteById(card.getId());
-  }
-
-  public Card findCard(Long cardId) {
-    return cardRepository.findById(cardId).orElseThrow(() ->
-        new CustomException(CustomErrorCode.CARD_NOT_FOUND, null));
-  }
-
-  @Override
-  @Transactional
-  public List<CardMemberResponseDto> changeCardMembers(Long cardId, String emailList) {
-    List<CardMemberResponseDto> result = new ArrayList<>();
-    Card nowCard = findCard(cardId);
-    // 전체삭제
-    nowCard.getCardMembers().clear();
-    if (!emailList.isEmpty() && emailList != null) {
-      List<String> emailArrays = Arrays.stream(emailList.split(",")).toList();
-
-      for (int i = 0; i < emailArrays.size(); i++) {
-        Optional<User> optionalUser = userRepository.findByEmail(emailArrays.get(i));
-        if (optionalUser.isPresent()) {
-          CardMember cardMember = new CardMember(nowCard, optionalUser.get());
-          CardMember returnCardMember = cardMemberRepository.save(cardMember);
-          result.add(new CardMemberResponseDto(returnCardMember));
+        // 파일 등록
+        List<S3FileDto> fileDtoList = null;
+        if (!(files == null || (files.size() == 1 && files.get(0).isEmpty()))) {
+            fileDtoList = fileUploader.uploadFiles(files, "files");
+            requestDto.setAttachment(fileDtoList);
         }
-      }
+
+        Card card = Card.builder()
+                .requestDto(requestDto)
+                .deck(deck)
+                .build();
+        Card returnCard = cardRepository.save(card);
+
+        return new CardResponseDto(returnCard);
     }
-    return result;
-  }
 
-  @Override
-  @Transactional
-  public CardResponseDto changeDueDate(Long cardId, String dueDate) {
-    Card card = findCard(cardId);
-    LocalDateTime parseDueDate = LocalDateTime.parse(dueDate);
-    card.updateDueDate(parseDueDate);
-    CardResponseDto responseDto = new CardResponseDto(card);
-    return responseDto;
-  }
+    @Override
+    @Transactional
+    public CardResponseDto update(Long cardId, CardRequestDto requestDto, List<MultipartFile> files)
+            throws IOException {
+        // 파일첨부
+        List<S3FileDto> fileDtoList = null;
+        Card card = findCard(cardId);
 
-  @Override
-  @Transactional
-  public CardResponseDto moveCard(Long cardId, Long order) {
-    Card card = findCard(cardId);
-    cardRepository.changeOrder(card, order);
-    card = findCard(cardId);
-    card.updateParentId(order);
-    CardResponseDto responseDto = new CardResponseDto(card);
-    return responseDto;
-  }
+        // 파일정보 불러오기
+        List<S3FileDto> attachment = card.getAttachment();
 
-  @Override
-  @Transactional(readOnly = true)
-  public List<CardSearchResponseDto> searchLabelList(Long labelId, int page, int size,
-      String sortBy,
-      boolean isAsc) {
-    Direction direction = isAsc ? Direction.ASC : Direction.DESC;
-    Sort sort = Sort.by(direction, sortBy);
-    Pageable pageable = PageRequest.of(page, size, sort);
-    List<CardSearchResponseDto> cardList = cardRepository.findAllByLabelId(labelId, pageable);
-    return cardList;
-  }
+        // 기존 파일 삭제
+        if (attachment != null && !attachment.isEmpty()) {
+            for (S3FileDto s3FileDto : attachment) {
+                fileUploader.deleteFile(s3FileDto.getUploadFilePath(),
+                        s3FileDto.getUploadFileName());
+            }
+        }
 
-  @Override
-  @Transactional
-  public CardResponseDto moveCardToOtherDeck(Long deckId, Long cardId, Long order) {
-    Deck deck = findDeck(deckId);
-    Card card = findCard(cardId);
-    card = card.updateDeckAndParentId(deck, order);
-    // 다른 카드 들의 정렬 순서 변경
-    cardRepository.changeOrder(card, order);
-    CardResponseDto responseDto = new CardResponseDto(card);
+        // 파일 등록
+        if (!(files == null || (files.size() == 1 && files.get(0).isEmpty()))) {
+            fileDtoList = fileUploader.uploadFiles(files, "files");
+            requestDto.setAttachment(fileDtoList);
+        }
 
-    return responseDto;
-  }
+        // card 내용 수정
+        card.update(requestDto);
 
-  // 보드가 있는지 확인
-  private Board findBoard(Long boardId) {
-    return boardRepository.findById(boardId).orElseThrow(()
-        -> new CustomException(CustomErrorCode.BOARD_NOT_FOUND, null));
-  }
+        return new CardResponseDto(card);
+    }
 
-  // 덱이 있는지 확인
-  private Deck findDeck(Long id) {
-    return deckRepository.findById(id).orElseThrow(() ->
-        new CustomException(CustomErrorCode.DECK_NOT_FOUND, null)
-    );
-  }
+    @Override
+    @Transactional
+    public void delete(Long cardId) {
+        Card card = findCard(cardId);
+
+        // 파일정보 불러오기
+        List<S3FileDto> attachment = card.getAttachment();
+
+        // 기존 파일 삭제
+        if (attachment != null && !attachment.isEmpty()) {
+            for (S3FileDto s3FileDto : attachment) {
+                fileUploader.deleteFile(s3FileDto.getUploadFilePath(),
+                        s3FileDto.getUploadFileName());
+            }
+        }
+        //기존 체크리스트 삭제
+        for (int i = 0; i < card.getCheckLists().size(); i++) {
+            checkListService.delete(card.getCheckLists().get(i).getId());
+        }
+
+        cardRepository.deleteById(card.getId());
+    }
+
+    public Card findCard(Long cardId) {
+        return cardRepository.findById(cardId).orElseThrow(() ->
+                new CustomException(CustomErrorCode.CARD_NOT_FOUND, null));
+    }
+
+    @Override
+    @Transactional
+    public List<CardMemberResponseDto> changeCardMembers(Long cardId, String emailList) {
+        List<CardMemberResponseDto> result = new ArrayList<>();
+        Card nowCard = findCard(cardId);
+        // 전체삭제
+        nowCard.getCardMembers().clear();
+        if (!emailList.isEmpty()) {
+            List<String> emailArrays = Arrays.stream(emailList.split(",")).toList();
+
+            for (String emailArray : emailArrays) {
+                Optional<User> optionalUser = userRepository.findByEmail(emailArray);
+                if (optionalUser.isPresent()) {
+                    CardMember cardMember = new CardMember(nowCard, optionalUser.get());
+                    CardMember returnCardMember = cardMemberRepository.save(cardMember);
+                    result.add(new CardMemberResponseDto(returnCardMember));
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    @Transactional
+    public CardResponseDto changeDueDate(Long cardId, String dueDate) {
+        Card card = findCard(cardId);
+        LocalDateTime parseDueDate = LocalDateTime.parse(dueDate);
+        card.updateDueDate(parseDueDate);
+
+        return new CardResponseDto(card);
+    }
+
+    @Override
+    @Transactional
+    public CardResponseDto moveCard(Long cardId, Long order) {
+        Card card = findCard(cardId);
+        cardRepository.changeOrder(card, order);
+
+        card.updateParentId(order);
+
+        return new CardResponseDto(card);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<CardSearchResponseDto> searchLabelList(Long labelId, int page, int size, String sortBy, boolean isAsc) {
+        Direction direction = isAsc ? Direction.ASC : Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return cardRepository.findAllByLabelId(labelId, pageable);
+    }
+
+    @Override
+    @Transactional
+    public CardResponseDto moveCardToOtherDeck(Long deckId, Long cardId, Long order) {
+        Deck deck = findDeck(deckId);
+        Card card = findCard(cardId);
+        card = card.updateDeckAndParentId(deck, order);
+        // 다른 카드 들의 정렬 순서 변경
+        cardRepository.changeOrder(card, order);
+
+        return new CardResponseDto(card);
+    }
+
+    // 보드가 있는지 확인
+    private Board findBoard(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(()
+                -> new CustomException(CustomErrorCode.BOARD_NOT_FOUND, null));
+    }
+
+    // 덱이 있는지 확인
+    private Deck findDeck(Long id) {
+        return deckRepository.findById(id).orElseThrow(() ->
+                new CustomException(CustomErrorCode.DECK_NOT_FOUND, null)
+        );
+    }
 }
