f7818fd3c9200c088e36ccb9ac151984a9e5e223
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-14 13:50:42 +0900

    [update] CheckList - Checks API 기능 수정

--- src/main/java/com/example/itwassummer/check/controller/ChecksController.java
+++ src/main/java/com/example/itwassummer/check/controller/ChecksController.java
@@ -10,15 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
